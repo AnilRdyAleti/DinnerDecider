39d315f5d44221ae04b3f05a571c129344a6eba2
AnilRdyAleti <AnilRdyAleti@github.com>
2018-09-04 17:50:06 -0500

    Recreated main activity

--- app/src/main/java/com/example/anilreddy/dinnerdecider/MainActivity.kt
+++ app/src/main/java/com/example/anilreddy/dinnerdecider/MainActivity.kt
@@ -2,14 +2,30 @@ package com.example.anilreddy.dinnerdecider
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.example.anilreddy.dinnerdecider.R.id.btn_dinner_decider
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
+    private val food = arrayListOf("Chinese", "Hamburger", "Pizza", "McDonalds", "Barros Pizza")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        food_text.text = "Burger King"
+        btn_dinner_decider.setOnClickListener {
+
+            val random = Random()
+            val randomFood = random.nextInt(food.count())
+
+            food_text.text = food[randomFood]
+        }
+
+        btn_add_food.setOnClickListener {
+
+            val newFood = et_add_new_food.text.toString()
+            food_text.text = newFood
+        }
+        
     }
 }

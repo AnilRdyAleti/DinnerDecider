9022e79ee5ba0f281eb863eb62ffb1606f62add6
AnilRdyAleti <AnilRdyAleti@github.com>
2018-09-05 12:42:19 -0500

    Created two classes for variable and sample kotlin file

---
+++ app/src/main/java/com/example/anilreddy/dinnerdecider/HelloKotlin.kt
@@ -0,0 +1,5 @@
+package com.example.anilreddy.dinnerdecider
+
+fun main(args: Array<String>) {
+    print("Hello Kotlin")
+}
---
+++ app/src/main/java/com/example/anilreddy/dinnerdecider/Variables.kt
@@ -0,0 +1,21 @@
+package com.example.anilreddy.dinnerdecider
+
+fun main(args: Array<String>) {
+    //mutable variable
+    var intNum = 1
+    var doubleNum = 10.55
+    var floatNum = 10.66f
+
+    var stringVal = "Value"
+
+    var booleanVal = true
+
+    //immutable variable
+    val num = 1;
+
+    //declare and initialize later
+    val value: Int
+    value = 3
+
+
+}

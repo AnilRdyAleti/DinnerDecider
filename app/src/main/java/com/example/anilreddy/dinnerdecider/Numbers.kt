fe2992001da1eaa5ffedab60ed3f6937bf149182
AnilRdyAleti <AnilRdyAleti@github.com>
2018-09-05 14:26:19 -0500

    Added Functions and Numbers classes

---
+++ app/src/main/java/com/example/anilreddy/dinnerdecider/Numbers.kt
@@ -0,0 +1,12 @@
+package com.example.anilreddy.dinnerdecider
+
+fun main(args: Array<String>){
+    var a = 55
+    var b = 25
+
+    println(a + b)
+    println(a - b)
+    println(a / b)
+    println(a * b)
+    println(a % b)
+}

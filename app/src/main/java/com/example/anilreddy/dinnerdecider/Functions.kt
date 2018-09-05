7e851607b9c16a8ceb281090563e7934a92b967d
AnilRdyAleti <AnilRdyAleti@github.com>
2018-09-05 15:02:10 -0500

    created returning functions

--- app/src/main/java/com/example/anilreddy/dinnerdecider/Functions.kt
+++ app/src/main/java/com/example/anilreddy/dinnerdecider/Functions.kt
@@ -1,21 +1,22 @@
 package com.example.anilreddy.dinnerdecider
 
-fun main(args: Array<String>){
+fun main(args: Array<String>) {
 
-    fun printString(){
+    fun printString() {
         println("Hello this is Sample Function Examples")
     }
 
     printString()
 
-    fun printString(line: String){
+    fun printString(line: String) {
         println(line)
     }
 
     printString("Hello this is a String value")
 
-
-    fun  addNumber( a: Int, b: Int) {
-
+    fun addNumber(a: Int, b: Int): Int {
+        return a + b;
     }
+
+    println(addNumber(5,10))
 }

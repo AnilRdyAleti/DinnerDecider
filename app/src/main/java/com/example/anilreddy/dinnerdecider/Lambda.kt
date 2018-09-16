6d0597917be7d683a33ed1c09e93f89d184ea204
AnilRdyAleti <AnilRdyAleti@github.com>
2018-09-15 22:40:31 -0500

    updated Lambda and player files

---
+++ app/src/main/java/com/example/anilreddy/dinnerdecider/Lambda.kt
@@ -0,0 +1,10 @@
+package com.example.anilreddy.dinnerdecider
+
+val printMessage = { msg: String -> println(msg) }
+
+val Sum = { x: Int, y: Int -> x + y }
+
+fun main(args: Array<String>) {
+    printMessage("Hola, Hola........")
+    println(Sum(3 ,9))
+}
